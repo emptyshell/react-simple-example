4072807de5f63756f139ab646f3a524da01717ee
valentin <valentin@suruceanu.net>
2023-07-22 12:59:16 +0300

    fixed minor issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,23 +6,14 @@ import './header.css';
  * @returns
  */
 export default function Header({setPage}) {
-  /**
-   * handle
-   * @param {*} page page
-   */
-  function handlePage(page) {
-    console.log(page);
-    setPage(page);
-  };
-
   return (
     <header className="header">
       <h1>My react app</h1>
       <div className='navigation'>
-        <a href="#" onClick={handlePage('products')}>
+        <a href="#" onClick={() => setPage('products')}>
           Products
         </a>
-        <a href="#" onClick={handlePage('copywriters')}>
+        <a href="#" onClick={() => setPage('copywriters')}>
           Copywrites
         </a>
       </div>
